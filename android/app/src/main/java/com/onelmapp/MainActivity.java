b7be3e610e24f7a545fe0054d8da3f7502317ac5
Sameer Qidwai <samiqidwai@gmail.com>
2022-10-05 14:34:01 +0500

    Splash Screen done

--- android/app/src/main/java/com/onelmapp/MainActivity.java
+++ android/app/src/main/java/com/onelmapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.onelmapp;
 
+import android.os.Bundle; // here
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
 
@@ -14,6 +18,12 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "onelmApp";
   }
+  
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
